68bc8c5050485f590d55b483cd8621dda0529922
Andrea Heilrath <andrea.heilrath@gmail.com>
2021-07-20 13:28:31 +0200

    removed class, now all based not networkx graph, no more global variables, graph is saved as json in dcc.store, added hidden table for state variables

--- main.py
+++ main.py
@@ -2,24 +2,22 @@
 import sys
 
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
 import plotly.graph_objects as go
-import knowledge_network as kn
+import wiki_network
 
 
 #===============================================================================
 # basic stuff & global variables
 
-# load external syle cheet (css)
-#external_stylesheets = ['style.css']#['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 # create dash app
-app = dash.Dash(__name__) #, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
-# check if running in local mode (-l)
+# check if running in local mode (-local)
 if not "-local" in sys.argv:
     server = app.server
 
@@ -30,9 +28,6 @@ else:
     base_url = "https://en.wikipedia.org/wiki/"
     local = False
 
-# define graph graph object
-graph = None
-start_layout = 'kamada_kawai_layout'
 
 #===============================================================================
 # generate and style figure
@@ -47,8 +42,9 @@ fig = go.Figure(data = None,  # hier wird die Funktion von oben benutzt
                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
                 )
 
+
 #===============================================================================
-# Define App Layout
+# define app layout
 
 app.layout = html.Div(className="app",
     children = [
@@ -57,9 +53,9 @@ app.layout = html.Div(className="app",
     html.P("This app generates a graph based on links between Wikipedia articles. "),
     html.P(["You can determine the article to start with.",
     " All linked articles in the article's summary will be added to the graph. ",
-    "If the depth is greater than one, the links of the linked articles will be added and so on ... ",
+    "If the depth is greater than one, the links of the linked articles will be added, and so on ... ",
     html.Br(),
-    "The graph shows all articles as bubbles and all links as lines."]),
+    "The figure shows all articles as bubbles and all links as lines."]),
     ]),
     html.Div(className = "input-container", children =[
     dcc.Input(className="input", id='topic', type='text', value='Python (programming language)',\
@@ -82,73 +78,80 @@ app.layout = html.Div(className="app",
             {'label': 'Spectral Layout', 'value': 'spectral_layout'},
             {'label': 'Spiral Layout', 'value': 'spiral_layout'}
         ],
-        value=start_layout),
-    dcc.Markdown(id='text_output')
-    ])
+        value='kamada_kawai_layout'),
+    dcc.Markdown(id='summary', style = {'color' : 'black'}),
+    dash_table.DataTable(id='table',
+    columns=[{"name": "param", "id": "param"}, {"name": "value", "id": "value"}],
+    data= [
+        	{"param": "topic", 'value': None},
+            {"param": "depth", 'value': None},
+            {"param": "slug", 'value': None}]),
+    dcc.Store(id='graph_container')
+    ]),
 ])
 
 
-
 #===============================================================================
-# Callbacks - Interaktion mit der Benutzeroberfläche
+# callbacks
 
-# Mit @ wird ein sogenannter "Decorater" benutzt.
-# Der Decorator führt dazu, dass die Funktion, die darunter deklariert wird
-# (hier update_output) die Funktionalität des Decorators erhält.
-# Hier werden Input, Output, State vom Paket dash.dependencies verwendet (siehe import).
-# Wichtig ist, dass Inputs und Outputs mit den Argumenten und Returns der Funktion zusammenpassen!
-
-
-@app.callback(Output('text_output', 'children'),
+@app.callback(Output('summary', 'children'),
+              Output('summary', 'style'),
+              Output('table', 'data'),
               Input('start_button', 'n_clicks'),
               State('topic', 'value'),
               State('depth', 'value'))
 
 def check_if_article_exists(n_clicks, topic, depth):
-    global graph
-    graph = kn.KnowledgeGraph(topic, depth, start_layout, base_url, local)
-
-    if graph.topic:
-        output = '''
-            The current topic is **{}**
-            and the depth of the network is **{}**.
-        '''.format(topic, depth)
-
+    article, slug = wiki_network.valid_article(topic)
+    if article:
+        summary = '''
+            Building graph around **{}** with depth **{}**.
+            This can take a while!
+        '''.format(article, depth)
+        style = {'color' : 'black'}
+        data = [{'param': 'topic', 'value': article},
+                {'param': 'depth', 'value': depth},
+                {'param': 'slug', 'value': slug}]
     else:
-        output = "**Topic does not exist.**"
+        summary = "**Topic does not exist.**"
+        style = {'color' : 'red'}
+        data= [
+            	{"param": "topic", 'value': None},
+                {"param": "depth", 'value': None},
+                {"param": "slug", 'value': None}]
 
-    return output
+    return summary, style, data
 
 
 @app.callback(Output('knowledge-graph', 'figure'),
-              Input('text_output', 'children'),
+              Output('graph_container', 'data'),
+              Input('table', 'data'),
               Input('layout_dropdown', 'value'),
               State('knowledge-graph', 'figure'),
-              State('layout_dropdown', 'value'))
+              State('layout_dropdown', 'value'),
+              State('graph_container', 'data'))
 
-def update_figure(text, dropdown, figure, layout):
-    #print(text, dropdown, figure, layout)
+def update_figure(data, dropdown, figure, layout, json_data):
     ctx = dash.callback_context
     input_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    global graph
 
     if input_id == "layout_dropdown":
-        graph.layout = ctx.triggered[0]["value"]
-        figure['data'] = graph.display()
-        display = 'Changing to ' + layout
-        return figure
-    else:
-        if text == '**Topic does not exist.**':
-            figure['data'] = None
-        else:
-            graph.build()
-            figure['data'] = graph.display()
+        figure['data'] = wiki_network.display(json_data, ctx.triggered[0]["value"])
+        return figure, json_data
 
-        return figure
+    else:
+        if data[0]['value'] == None:
+            figure['data'] = None
+            json_graph = None
+        else:
+            json_graph = wiki_network.build(data[0]['value'], data[1]['value'], data[2]['value'], local, base_url)
+            figure['data'] = wiki_network.display(json_graph, layout)
+
+        return figure, json_graph
 
 
 #===============================================================================
-# Server starten
+# start server
 
 if __name__ == '__main__':
     app.run_server(debug=True, port = 8000)
---
+++ wiki_network.py
@@ -0,0 +1,131 @@
+import requests
+from bs4 import BeautifulSoup
+import networkx as nx
+import plotly.graph_objects as go
+
+
+def build(topic, depth, slug, local, base_url):
+
+    self = nx.DiGraph()
+
+    self.add_node(topic)
+    self.nodes[topic]['slug'] = slug
+
+    def add_links(origin, depth):
+        links = get_links(self.nodes[origin]['slug'])
+        for topic in links:
+            if topic not in self:
+                self.add_node(topic)
+                self.nodes[topic]['slug'] = links[topic]
+            self.add_edge(origin, topic)
+            if depth > 1:
+                add_links(topic, depth-1)
+
+
+    def get_links(slug):
+
+        def to_dict(links, shift=0):
+            link_dict = {}
+            for link in links:
+                try:
+                    link_dict[link['title']] = link['href'][shift:]
+                except KeyError:
+                    pass
+            return link_dict
+
+        page = requests.get(base_url + slug)
+        soup = BeautifulSoup(page.content, features = 'lxml')
+        article = soup.h1.string
+
+        links = []
+        elements = soup.find_all(['p', 'h2'])
+        for e in elements:
+            if e.name == 'h2': break
+            links.extend(e.find_all('a'))
+
+        if local:
+            link_dict = to_dict(links)
+        else:
+            link_dict = to_dict(links, 6)
+
+        return link_dict
+
+    add_links(topic, depth)
+
+    return nx.node_link_data(self)
+
+
+def display(json_graph, layout):
+
+    self = nx.node_link_graph(json_graph)
+
+    layout_dict = {'kamada_kawai_layout': nx.kamada_kawai_layout,
+                        'shell_layout' : nx.shell_layout,
+                        'spring_layout': nx.spring_layout,
+                        'spectral_layout': nx.spectral_layout,
+                        'spiral_layout': nx.spiral_layout
+                        }
+
+    layout = layout_dict[layout]
+
+    pos = layout(self)
+
+    centrality = nx.degree_centrality(self)
+    node_x, node_y, marker_size, node_text = [], [], [], []
+
+    for node in self.nodes():          # hier werden Knoten aus dem Graph extrahiert
+        node_x.append(pos[node][0])
+        node_y.append(pos[node][1])
+        marker_size.append(pow(centrality[node], 0.5)*100)
+        node_text.append(str(node))
+
+    node_adjacencies = []
+    for node, adjacencies in enumerate(self.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+
+
+    node_trace = go.Scatter(         # erzeugt den Plot der die Knoten darstellt
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        text=node_text,
+        marker=dict(showscale=True, colorscale='YlGnBu', reversescale=True,
+                    size=marker_size,
+                    color=node_adjacencies,
+                    colorbar=dict(thickness=15, title='Node Connections',
+                                  xanchor='left', titleside='right'),
+                    line_width=2))
+
+    edge_x, edge_y = [], []
+    for edge in self.edges():
+        edge_x.extend([pos[edge[0]][0], pos[edge[1]][0], None])
+        edge_y.extend([pos[edge[0]][1], pos[edge[1]][1], None])
+
+    edge_trace = go.Scatter(x=edge_x, y=edge_y,
+                            line=dict(width=0.5, color='#888'))
+
+    return [edge_trace, node_trace]
+
+
+def valid_article(article):
+    page = requests.get("https://en.wikipedia.org/w/index.php?search=" + article)
+    url = str(page.url)
+    slug = url[url.rfind('/')+1:]
+    soup = BeautifulSoup(page.content, features = 'lxml')
+    page_title = soup.h1.string
+    if article.lower() == page_title.lower():
+        return page_title, slug
+    else:
+        return None, None
+
+
+'''
+    def valid(topic):
+        for expression in self.exclude_start:
+            if topic.find(expression) == 0:
+                return False
+        for expression in self.exclude_any:
+            if topic.find(expression) > 0:
+                return False
+        return True
+'''
